a5975692f158b35dde40e74b4b32afc1b5a5b727
Trevor <trevor@Trevors-MacBook-Pro.local>
2022-07-28 14:38:23 -0400

    form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,18 +1,147 @@
 import React from 'react'
 import './contact.scss'
+import { useState, useRef } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+  const [message, setMessage] = useState('')
+  const [color, setColor] = useState('')
+  const form = useRef()
+
+  // let buttonStyle = {"background": {color}}
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    emailjs
+			.sendForm(
+				'service_xbn6upm',
+				'template_ljeonjo',
+				form.current,
+				'Wkk0dwkyDYusWyMCW'
+			)
+			.then(
+				(result) => {
+					console.log(result.text)
+				},
+				(error) => {
+					console.log(error.text)
+				}
+			)
+      }
+
   return (
-    <section id='contact'>
-      <h1 className='section-title '>Let's Keep in Touch!</h1>
-      <div className='contact-container'>
-        <a href="mailto:trevorlomba@gmail.com" rel="noopener noreferrer" className="contact-link" target="_blank">email</a>
-        <a href="tel:5084412186" rel="noopener noreferrer" className="contact-link" target="_blank" >phone</a>
-        <a href="https://github.com/trevorlomba" rel="noopener noreferrer" className="contact-link" target="_blank">github</a>
-        <a href="https://www.linkedin.com/in/trevorlomba/" rel="noopener noreferrer" className="contact-link" target="_blank">linkedin</a>
-      </div>
-    </section>
-  )
+		<>
+			<section id='contact'>
+				<div className='contact-wrapper'>
+					<h1 className='section-title '>Let's Keep in Touch!</h1>
+					<div className='contact-container'>
+						<a
+							href='mailto:trevorlomba@gmail.com'
+							rel='noopener noreferrer'
+							className='contact-link'
+							target='_blank'>
+							email
+						</a>
+						<a
+							href='tel:5084412186'
+							rel='noopener noreferrer'
+							className='contact-link'
+							target='_blank'>
+							phone
+						</a>
+						<a
+							href='https://github.com/trevorlomba'
+							rel='noopener noreferrer'
+							className='contact-link'
+							target='_blank'>
+							github
+						</a>
+						<a
+							href='https://www.linkedin.com/in/trevorlomba/'
+							rel='noopener noreferrer'
+							className='contact-link'
+							target='_blank'>
+							linkedin
+						</a>
+					</div>
+
+					<div className='my-8'>
+						<Form className='form-container ' onSubmit={sendMessage}>
+							<div>
+								<Form.Group controlId='name' className='label-container'>
+									<Form.Label className='label'>name</Form.Label>
+									<Form.Control
+										require
+										size='lg'
+										rows={3}
+										type='name'
+										value={name}
+										placeholder='Enter Name'
+										onChange={(event) => setName(event.target.value)}
+									/>
+								</Form.Group>
+							</div>
+							<div>
+								<Form.Group controlId='email' className='label-container'>
+									<Form.Label className='label'>email</Form.Label>
+									<Form.Control
+										require
+										size='lg'
+										rows={3}
+										type='email'
+										value={email}
+										placeholder='Enter Email'
+										onChange={(event) => setEmail(event.target.value)}
+									/>
+								</Form.Group>
+							</div>
+							<div>
+								<Form.Group controlId='message' className='label-container'>
+									<Form.Label className='label'>message</Form.Label>
+
+									<Form.Control
+										require
+										as='textarea'
+										size='lg'
+										rows={3}
+										type='message'
+										value={message}
+										placeholder='Enter Message'
+										onChange={(event) => setMessage(event.target.value)}
+									/>
+								</Form.Group>
+							</div>
+							<div>
+								<Form.Group
+									controlId='favorite-color'
+									className='label-container'>
+									<Form.Label className='label'>
+										favorite color{color}
+									</Form.Label>
+									<Form.Control
+										className='color-picker'
+										type='color'
+										controlId='favoriteColor'
+										defaultValue='#227093'
+										title='Choose your color'
+										onChange={(event) => setColor(event.target.value)}
+									/>
+								</Form.Group>
+							</div>
+							<div className='d-grid gap-2'>
+								<Button className='formButton' type='submit'>
+									Submit form
+								</Button>
+							</div>
+						</Form>
+					</div>
+				</div>
+			</section>
+		</>
+	)
 }
 
 export default Contact
