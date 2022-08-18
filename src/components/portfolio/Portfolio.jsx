d7ac8f9618fc1d26f9ec6b50b3eeb11ed579ce18
Trevor Lomba <76894056+trevorlomba@users.noreply.github.com>
2022-08-17 22:22:17 -0400

    talkmore

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -10,12 +10,12 @@ const Portfolio = (props) => {
 			<h1 className='section-title '>Portfolio</h1>
 			<Projects project={music} experience={props.experience} />
 			<Projects project={metroTrainer} experience={props.experience} />
+			<Projects project={talkMore} experience={props.experience} />
 			<Projects project={portfolio} experience={props.experience} />
 			<Projects project={scientificCalculator} experience={props.experience} />
 			<Projects project={xsAndOhs} experience={props.experience} />
 			<Projects project={basicCalculator} experience={props.experience} />
 			<Projects project={myPlaylist} experience={props.experience} />
-			<Projects project={talkMore} experience={props.experience} />
 		</section>
 	)
 }
