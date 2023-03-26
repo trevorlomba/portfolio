524d508429ba9344145f4271e90115c2bf52d4ff
Trevor Lomba <76894056+trevorlomba@users.noreply.github.com>
2023-03-25 22:54:38 -0400

    styling

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,12 +3,18 @@ import DownloadCTA from './cta-download/DownloadCTA'
 
 const CTA = () => {
   return (
-    <div className='cta headerbtn'>
-      <DownloadCTA />
-      <a href='#contact' className='btn headerbtn'>Contact</a>
-      <a href='http://linked.in/trevorlomba' className='btn headerbtn'>LinkedIn</a>
-    </div>
-  )
+		<div className='cta headerbtn'>
+			<DownloadCTA />
+			<a href='#contact' className='btn headerbtn'>
+				Contact
+			</a>
+			<a
+				href='https://www.linkedin.com/in/trevorlomba/' target="_blank"
+				className='btn headerbtn'>
+				LinkedIn
+			</a>
+		</div>
+	)
 }
 
 export default CTA
